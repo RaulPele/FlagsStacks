735bc0c1998afe688e78cebaa2e97178b3881f64
Raul Pele <raul.pele2001@gmail.com>
2022-07-25 15:33:34 +0300

    Initial Commit

---
+++ FlagsStacks/FlagsStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsStacksApp.swift
+//  FlagsStacks
+//
+//  Created by Raul Pele on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagsStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
